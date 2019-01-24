67e908f64e54ca118e6b68f29b564f6eb267fd0a
Smile <sunliangzesmile@gimal.com>
2019-01-24 15:07:11 +0800

    '初始化项目'

---
+++ src/main/java/com/smilecloud/pay/SmilecloudPayApplication.java
@@ -0,0 +1,14 @@
+package com.smilecloud.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmilecloudPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmilecloudPayApplication.class, args);
+	}
+
+}
+
